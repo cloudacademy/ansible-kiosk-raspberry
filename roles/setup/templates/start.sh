4e000dccfca153fbfa37967a414b65e8aa1c61a5
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-03-04 11:07:30 +0100

    switch from midori to iceweasel

--- roles/setup/templates/start.sh
+++ roles/setup/templates/start.sh
@@ -2,7 +2,7 @@
 
 sleep 120
 
-midori -e Fullscreen -a https://google.com
+iceweasel https://radiator:comswisscom@radiator.scapp.io
 unclutter -idle 5 -jitter 5
 
 sudo echo `date +"%Y/%m/%d-%H:%M:%S"`" : App Start" >> ~/Desktop/cronlog.log
